2dcb34f13c48b5f79bbf9b0e314355bfedbb70cb
pranithyelimala <pranithyelimala@gmail.com>
2025-11-05 16:04:17 +0530

    hi

--- task_2.py
+++ task_2.py
@@ -1,7 +1,7 @@
 num = int(input("please enter your age for authentication: "))
-if num%2==0 and num!=0:
+if num%2==0 and num!=0 and num >=18:
     print("Is positive and has right to vote: ",num)
-elif num%2!=0 and num !=0:
+elif num%2!=0 and num !=0 and num>=18:
     print("Is negitive number and has right to vote: ", num)
 else:
-    print("enter valid number: ")
+    print("Not a valid age: ")
